d0a9af68dd17e2b371ca23b380025adb79dc7a89
kacper <wotstyle@onet.pl>
2023-02-10 15:21:12 +0100

    Third update

--- new-pzaw-repo/src/UI/Card.jsx
+++ new-pzaw-repo/src/UI/Card.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from './Card.module.css';
 
 const Card = (props) => {
     return(
-        <div>
+        <div className={`${props.className} ${classes.card}`}>
             {props.children}
         </div>
     );
--- new-pzaw-repo/src/components/AddUser.jsx
+++ new-pzaw-repo/src/components/AddUser.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 function AddUser(){
     return(
-        <form>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text"></input>
+        <Card className={classes.input}>
+            <form>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"></input>
 
-            <label htmlFor="age">Age</label>
-            <input id="age" type="Number"></input>
+                <label htmlFor="age">Age</label>
+                <input id="age" type="Number"></input>
 
-            <button type="submit">Add User</button>
-        </form>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     );
 }
 
