0fde1f46a8dff3cdbfd9da87fa864cefb11419cd
kacper <wotstyle@onet.pl>
2023-02-16 15:19:44 +0100

    Add Button

--- new-pzaw-repo/src/components/AddUser.jsx
+++ new-pzaw-repo/src/components/AddUser.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from '../UI/Card';
+import Button from "../UI/Button";
 import classes from './AddUser.module.css';
 
 function AddUser(){
+
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
+    function nameChangeHandler(event){
+        setName(event.target.value)
+    }
+
+    function ageChangeHandler(event){
+        setAge(event.target.value)
+    }
+
+    function addUserHandler(event){
+        event.preventDefault();
+        console.log(name, age)
+        setAge('')
+        setName('')
+    }
+
     return(
         <Card className={classes.input}>
-            <form>
+            <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text"></input>
+                <input id="username" type="text" onChange={nameChangeHandler} value={name}></input>
 
                 <label htmlFor="age">Age</label>
-                <input id="age" type="Number"></input>
+                <input id="age" type="Number" onChange={ageChangeHandler} value={age}></input>
 
-                <button type="submit">Add User</button>
+                <Button type="submit">Add user</Button>
             </form>
         </Card>
     );
